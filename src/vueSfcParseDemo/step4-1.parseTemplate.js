98a60f1e7d96c233216b0b4f2f2e2aaf6aa320e5
leobod <leobod@dingtalk.com>
2023-12-04 17:58:12 +0800

    feat: demo to process template

--- src/vueSfcParseDemo/step4-1.parseTemplate.js
+++ src/vueSfcParseDemo/step4-1.parseTemplate.js
@@ -11,17 +11,84 @@ const fileContent = fs.readFileSync(filePath, "utf-8");
 // 解析.vue文件
 const parsedComponent = compiler.parseComponent(fileContent);
 
-
 // 获取模板部分
 const template = parsedComponent.template;
-console.log(template)
+// console.log(template.content)
 
 const $ = cheerio.load(template.content);
 // console.dir($)
-console.dir($._root)
+// console.dir($._root)
+// console.dir($._root.children)
 
-// 获取脚本部分
-const script = parsedComponent.script;
+// $._root.children[0] => html
+// console.dir($._root.children[0].children)
 
-// 获取样式部分
-const styles = parsedComponent.styles;
+// $._root.children[0].children[0] => head
+// $._root.children[0].children[1] => body
+// console.dir($._root.children[0].children[1].children)
+
+// $._root.children[0].children[1].children[0] => template下的首个元素
+// $._root.children[0].children[1].children[1] => template下的其他元素，一般应该是 text \n
+
+
+// console.dir($._root.children[0].children[1].children[0].children[1])
+/*
+<template v-if="isApprove">
+----------
+  name: 'template',
+  attribs: [Object: null prototype] { 'v-if': 'isApprove' },
+  type: 'tag',
+*/
+
+const tL1 = $._root.children[0].children[1].children[0].children[1]
+// console.dir(tL1.children[0].children[1])
+/*
+<div class="BtnGroup">
+----------
+  name: 'div',
+  attribs: [Object: null prototype] { class: 'BtnGroup' },
+  type: 'tag',
+  */
+
+// console.dir(tL1.children[0].children[1].children[1])
+/*
+<el-button
+    v-if="isAppoint"
+    type="primary"
+    size="small"
+    @click="onSettingSpecial"
+>
+    {{ pageType === 15 ? "指定分部负责人" : "指定人员" }}
+</el-button>
+----------
+  name: 'el-button',
+  attribs: [Object: null prototype] {
+    'v-if': 'isAppoint',
+    type: 'primary',
+    size: 'small',
+    '@click': 'onSettingSpecial'
+  },
+  type: 'tag',
+  children: [
+    Text {
+      parent: [Circular *2],
+      prev: null,
+      next: null,
+      startIndex: null,
+      endIndex: null,
+      data: '\n        {{ pageType === 15 ? "指定分部负责人" : "指定人员" }}\n      ',
+      type: 'text'
+    }
+  ],
+*/
+
+// const clickEls = $('[@click]');
+// console.log(clickEls)
+
+// clickEls.each((index, element) => {
+//     console.log($(element).html());
+// });
+
+const clickEls = $('[@click]');
+console.log(clickEls)
+
